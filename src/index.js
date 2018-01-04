2cd3dc1d23da162b11dfb43a9cac86ddc9c57034
Rocky Wu <rwu823@gmail.com>
2018-01-04 23:02:59 +0800

    Returns as Promise

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 const { exec } = require('child_process')
 const tStrings = require('./tStrings')
 
-const sh = (...args) => {
-  const cmds = tStrings(...args)
+const sh = (...args) =>
+  new Promise((resolve) => {
+    const cmds = tStrings(...args)
+    exec(cmds, (err, stdout, stderr) => {
+      if (err) {
+        throw new Error(err)
+      }
+      const output = stderr || stdout
 
-  exec(cmds, (err, stdout, stderr) => {
-    if (err) {
-      throw new Error(err)
-    }
-
-    console.log(stderr || stdout)
+      console.log(output)
+      resolve(output)
+    })
   })
-}
 
 module.exports = sh
